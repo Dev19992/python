4c67654c430f06dc1e72e5f2e964dedd7e0c4c18
devdd <d.raj.pathania@gmail.com>
2023-01-08 10:10:27 +0530

    lists added

---
+++ dictionary.py
@@ -0,0 +1,14 @@
+# A dictionary is a key value pair
+#key--->Value
+
+#a key can not be repetiable but value can be .
+#A dictionary is mutable
+#insertion order is preserved
+#crud operations can be implemented
+#indexing and slicing are not allowed
+
+#
+
+dict3={"name":"devansh","class":"btech","salary":570000}
+print(dict3)
+print(len(dict3))
